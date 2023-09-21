aa0040b5f50d121638c4430e2a1e7059d3d6446e
student <student@gnu.ac.in>
2023-09-21 08:53:55 +0530

    Practical 8

--- app/src/main/java/com/example/mad_practical_8_21012011049/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical_8_21012011049/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.mad_practical_8_21012011049
 
+import android.app.TimePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextClock
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.card.MaterialCardView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val cardView= findViewById<MaterialCardView>(R.id.card2)
+        val createalarm=findViewById<MaterialButton>(R.id.button_alarm)
+        createalarm.setOnClickListener{
+            TimePickerDialog(this, {tp,hour,minute -> setAlarmTime(hour, minute) },11,0,false).show()
+        }
+    }
+    fun setAlarmTime(hour:Int, minute:Int){
+        //cardView.visibility= View.GONE
     }
 }
